725e3d8f6ac7e79e96d4a6efeeab103da18647ea
devCarlosSantoss <carlosvinicius357890@gmail.com>
2025-01-22 12:06:18 -0300

    feat: Add backend project

---
+++ Backend/presente-especial/presente-especial/src/main/java/br/com/acmdevs/presente_especial/PresenteEspecialApplication.java
@@ -0,0 +1,13 @@
+package br.com.acmdevs.presente_especial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenteEspecialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenteEspecialApplication.class, args);
+	}
+
+}
